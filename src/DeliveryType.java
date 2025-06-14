32d0de32b27fea03aad7ca208ad36efac2b69e04
gDragon2077 <100093766+gDragon2077@users.noreply.github.com>
2025-06-15 01:06:09 +0300

    Delete src directory

--- src/DeliveryType.java
+++
@@ -1,6 +0,0 @@
-// DeliveryType.java - defineste metodele disponibile de livrare
-
-public enum DeliveryType {
-    STANDARD,   // livrare prin posta obisnuita
-    EXPRESS     // livrare rapida in 24 de ore
-}
